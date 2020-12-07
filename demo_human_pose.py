f9aaa16c0ac093793f9d163ee239378fd79fc224
LukasStu <72215368+LukasStu@users.noreply.github.com>
2020-12-07 17:37:03 +0100

    fixed PyramidROIAlign Output_shape
    https://github.com/aneeshchauhan/Mask_RCNN/commit/58333c4367fef536037287a7f29b30b4a406966f

---
+++ demo_human_pose.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 16:12:57 2020
+
+@author: LStue
+"""
+
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+import coco
+import utils
+import model as modellib
+import visualize
+from model import log
+
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "mylogs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco_humanpose.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+#COCO_DIR = "D:/Github/FastMaskRCNN/data/coco"  # TODO: enter value here
+#IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+COCO_DIR = "E:/DSLR/01_Privat/2020/Hochzeit/Daniel/Fotos_Reportage"  # TODO: enter value here
+IMAGE_DIR = COCO_DIR
+
+class InferenceConfig(coco.CocoConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    KEYPOINT_MASK_POOL_SIZE = 7
+
+inference_config = InferenceConfig()
+
+# Recreate the model in inference mode
+model = modellib.MaskRCNN(mode="inference", 
+                          config=inference_config,
+                          model_dir=MODEL_DIR)
+
+# Get path to saved weights
+
+model_path = os.path.join(ROOT_DIR, "mask_rcnn_coco_humanpose.h5")
+assert model_path != "", "Provide path to trained weights"
+print("Loading weights from ", model_path)
+model.load_weights(model_path, by_name=True)
